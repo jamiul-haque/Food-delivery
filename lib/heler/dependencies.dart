4dc34e28b1cc3bd009aae7c1b0a87e681fc9e820
Jamiul <jamiulal000@gmail.com>
2022-08-29 17:57:00 +0600

    users info

--- lib/heler/dependencies.dart
+++ lib/heler/dependencies.dart
@@ -9,15 +9,19 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../controllers/popular_product_controller.dart';
 import '../controllers/recommended_product_controller.dart';
+import '../controllers/user_controller.dart';
 import '../data/api/api_client.dart';
+import '../data/api/repository/user_repo.dart';
 
 Future<void> init() async {
   final sharedPreferences = await SharedPreferences.getInstance();
   Get.lazyPut(() => sharedPreferences);
   // api client
-  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL,sharedPreferences:Get.find()));
   Get.lazyPut(
       () => AuthRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+  Get.lazyPut(
+      () => UserRepo(apiClient: Get.find()));
 
   // repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
@@ -26,6 +30,7 @@ Future<void> init() async {
 
   // controllers
   Get.lazyPut(() => AuthController(authRepo: Get.find()));
+  Get.lazyPut(() => UserController(userRepo: Get.find()));
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
   Get.lazyPut(
       () => RecommendedProductController(recommendedProductRepo: Get.find()));
--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/base/custom_loader.dart';
 import 'package:food_delivery_app/controllers/auth_controller.dart';
 import 'package:food_delivery_app/controllers/cart_controller.dart';
+import 'package:food_delivery_app/controllers/user_controller.dart';
 import 'package:food_delivery_app/routes/route_helper.dart';
 import 'package:food_delivery_app/utils/colors.dart';
 import 'package:food_delivery_app/utils/dimensions.dart';
@@ -14,6 +16,10 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool _userLoggedIn = Get.find<AuthController>().UserLoggedIn();
+    if (_userLoggedIn) {
+      Get.find<UserController>().getUserInfo();
+    }
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -25,105 +31,171 @@ class AccountPage extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.only(top: Dimensions.height20),
-        width: double.maxFinite,
-        child: Column(
-          children: [
-            // profile icon
-            AppIcon(
-              icon: Icons.person,
-              backgroundColor: AppColors.mainColor,
-              iconColor: Colors.white,
-              iconSize: Dimensions.height45 + Dimensions.height30,
-              size: Dimensions.height15 * 10,
-            ),
-            SizedBox(height: Dimensions.height30),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    // name
-                    AccountWidget(
-                        appIcon: AppIcon(
-                          icon: Icons.person,
-                          backgroundColor: Colors.amber,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height10 * 5 / 2,
-                          size: Dimensions.height10 * 5,
-                        ),
-                        bigText: BigText(text: 'Jamiul')),
-                    SizedBox(height: Dimensions.height20),
-                    // phone
-                    AccountWidget(
-                        appIcon: AppIcon(
-                          icon: Icons.phone,
-                          backgroundColor: Colors.amber,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height10 * 5 / 2,
-                          size: Dimensions.height10 * 5,
-                        ),
-                        bigText: BigText(text: '01759152674')),
-                    SizedBox(height: Dimensions.height20),
-                    // email
-                    AccountWidget(
-                        appIcon: AppIcon(
-                          icon: Icons.email,
-                          backgroundColor: Colors.amber,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height10 * 5 / 2,
-                          size: Dimensions.height10 * 5,
-                        ),
-                        bigText: BigText(text: 'jamiulal000@gmail.coom')),
-                    SizedBox(height: Dimensions.height20),
-                    // address
-                    AccountWidget(
-                        appIcon: AppIcon(
-                          icon: Icons.location_on,
-                          backgroundColor: Colors.amber,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height10 * 5 / 2,
-                          size: Dimensions.height10 * 5,
-                        ),
-                        bigText: BigText(text: 'Fill in your address')),
-                    SizedBox(height: Dimensions.height20),
-                    // message
-                    AccountWidget(
-                        appIcon: AppIcon(
-                          icon: Icons.message_outlined,
-                          backgroundColor: Colors.amber,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height10 * 5 / 2,
-                          size: Dimensions.height10 * 5,
-                        ),
-                        bigText: BigText(text: 'write your message here')),
-                    SizedBox(height: Dimensions.height20),
-                    // message
-                    GestureDetector(
-                      onTap: () {
-                        if (Get.find<AuthController>().UserLoggedIn()) {
-                          Get.find<AuthController>().clearSharedData();
-                          Get.find<CartController>().clear();
-                          Get.find<CartController>().clearCartHistory();
-                          Get.offAllNamed(RouteHelper.getSignInPage());
-                        }
-                      },
-                      child: AccountWidget(
-                          appIcon: AppIcon(
-                            icon: Icons.logout_outlined,
-                            backgroundColor: Colors.amber,
+      body: GetBuilder<UserController>(
+        builder: (userController) {
+          return _userLoggedIn
+              ? (userController.isLoading
+                  ? Container(
+                      margin: EdgeInsets.only(top: Dimensions.height20),
+                      width: double.maxFinite,
+                      child: Column(
+                        children: [
+                          // profile icon
+                          AppIcon(
+                            icon: Icons.person,
+                            backgroundColor: AppColors.mainColor,
                             iconColor: Colors.white,
-                            iconSize: Dimensions.height10 * 5 / 2,
-                            size: Dimensions.height10 * 5,
+                            iconSize: Dimensions.height45 + Dimensions.height30,
+                            size: Dimensions.height15 * 10,
                           ),
-                          bigText: BigText(text: 'Logout')),
+                          SizedBox(height: Dimensions.height30),
+                          Expanded(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  // name
+                                  AccountWidget(
+                                      appIcon: AppIcon(
+                                        icon: Icons.person,
+                                        backgroundColor: Colors.amber,
+                                        iconColor: Colors.white,
+                                        iconSize: Dimensions.height10 * 5 / 2,
+                                        size: Dimensions.height10 * 5,
+                                      ),
+                                      bigText: BigText(
+                                          text: userController.userModel.name)),
+                                  SizedBox(height: Dimensions.height20),
+                                  // phone
+                                  AccountWidget(
+                                      appIcon: AppIcon(
+                                        icon: Icons.phone,
+                                        backgroundColor: Colors.amber,
+                                        iconColor: Colors.white,
+                                        iconSize: Dimensions.height10 * 5 / 2,
+                                        size: Dimensions.height10 * 5,
+                                      ),
+                                      bigText: BigText(
+                                          text:
+                                              userController.userModel.phone)),
+                                  SizedBox(height: Dimensions.height20),
+                                  // email
+                                  AccountWidget(
+                                      appIcon: AppIcon(
+                                        icon: Icons.email,
+                                        backgroundColor: Colors.amber,
+                                        iconColor: Colors.white,
+                                        iconSize: Dimensions.height10 * 5 / 2,
+                                        size: Dimensions.height10 * 5,
+                                      ),
+                                      bigText: BigText(
+                                          text:
+                                              userController.userModel.email)),
+                                  SizedBox(height: Dimensions.height20),
+                                  // address
+                                  AccountWidget(
+                                      appIcon: AppIcon(
+                                        icon: Icons.location_on,
+                                        backgroundColor: Colors.amber,
+                                        iconColor: Colors.white,
+                                        iconSize: Dimensions.height10 * 5 / 2,
+                                        size: Dimensions.height10 * 5,
+                                      ),
+                                      bigText: BigText(
+                                          text: 'Fill in your address')),
+                                  SizedBox(height: Dimensions.height20),
+                                  // message
+                                  AccountWidget(
+                                      appIcon: AppIcon(
+                                        icon: Icons.message_outlined,
+                                        backgroundColor: Colors.amber,
+                                        iconColor: Colors.white,
+                                        iconSize: Dimensions.height10 * 5 / 2,
+                                        size: Dimensions.height10 * 5,
+                                      ),
+                                      bigText: BigText(
+                                          text: 'write your message here')),
+                                  SizedBox(height: Dimensions.height20),
+                                  // message
+                                  GestureDetector(
+                                    onTap: () {
+                                      if (Get.find<AuthController>()
+                                          .UserLoggedIn()) {
+                                        Get.find<AuthController>()
+                                            .clearSharedData();
+                                        Get.find<CartController>().clear();
+                                        Get.find<CartController>()
+                                            .clearCartHistory();
+                                        Get.offAllNamed(
+                                            RouteHelper.getSignInPage());
+                                      }
+                                    },
+                                    child: AccountWidget(
+                                        appIcon: AppIcon(
+                                          icon: Icons.logout_outlined,
+                                          backgroundColor: Colors.amber,
+                                          iconColor: Colors.white,
+                                          iconSize: Dimensions.height10 * 5 / 2,
+                                          size: Dimensions.height10 * 5,
+                                        ),
+                                        bigText: BigText(text: 'Logout')),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : CustomLoader())
+              : Container(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: double.maxFinite,
+                          height: Dimensions.height20 * 8,
+                          margin: EdgeInsets.only(
+                              left: Dimensions.width20,
+                              right: Dimensions.width20),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(Dimensions.radius20),
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image:
+                                      AssetImage("assets/images/logo1.gif"))),
+                        ),
+                        SizedBox(height: Dimensions.height20),
+                        GestureDetector(
+                          onTap: () {
+                            Get.toNamed(RouteHelper.getSignInPage());
+                          },
+                          child: Container(
+                            width: double.maxFinite,
+                            height: Dimensions.height20 * 5,
+                            margin: EdgeInsets.only(
+                                left: Dimensions.width20,
+                                right: Dimensions.width20),
+                            decoration: BoxDecoration(
+                              color: AppColors.mainColor,
+                              borderRadius:
+                                  BorderRadius.circular(Dimensions.radius20),
+                            ),
+                            child: Center(
+                              child: BigText(
+                                text: "Sign in",
+                                color: Colors.white,
+                                size: Dimensions.font26,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+                  ),
+                );
+        },
       ),
     );
   }
